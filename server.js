d5c32d0d090b5f0b5c7c9d4baa579fe2a128fcc9
Ridel Saavedra Flores <ridel.saavedra@nfq.es>
2024-09-26 02:45:24 +0200

    update port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 app.use(limiter);
 app.use(express.json());
